d2b993c687caf9aa39dae60d6a7ceda4b7783d2c
Adrien Regis <adrien.regis@gmail.com>
2023-10-03 15:30:47 +0200

    web projects advanced with carousel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def web
+  end
+
+  def english
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "web", to: "pages#web"
+  get "english", to: "pages#english"
+  get "contact", to: "pages#contact"
 end
