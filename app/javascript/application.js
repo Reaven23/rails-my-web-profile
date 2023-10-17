254b1595e67eb518b888b681673bf91d8b88b9a6
Adrien Regis <adrien.regis@gmail.com>
2023-10-17 15:45:19 +0200

    add JS for classes in English page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,18 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+
+const sections = document.querySelectorAll('.section')
+
+// On click event for each section
+sections.forEach((section)=>{
+    section.addEventListener('click',()=>{
+    // remove active class from all another section
+    // and add it to the selected section
+        sections.forEach((section) => {
+            section.classList.remove('active')
+        })
+        section.classList.add('active')
+    })
+})
