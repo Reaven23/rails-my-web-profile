650a21f41a1dc531dc9d3347e6741efb4b48bf0d
Adrien Regis <adrien.regis@gmail.com>
2023-11-08 12:41:31 +0100

    rescue path for mailer when doesnt proceed

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -6,10 +6,15 @@ class ContactFormController < ApplicationController
     @email = params[:contact_form][:email]
     @message = params[:contact_form][:message]
 
-    # Perform any necessary actions with the form data
-    NotifierMailer.simple_message(@name, @last_name, @email, @message).deliver_now
-    flash[:success] = "Your message has been sent successfully."
-    redirect_to :root
+    begin
+      # Attempt to send the email
+      NotifierMailer.simple_message(@name, @last_name, @email, @message).deliver_now
+      flash[:success] = "Your message has been sent successfully."
+      redirect_to :root
+    rescue Errno::ECONNREFUSED => e
+      flash[:alert] = "An error occurred while sending the email. Please try again later."
+      redirect_to :root
+    end
   end
 
 end
