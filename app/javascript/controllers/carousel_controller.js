cb908fe61636fe3a633b197485d6d263e49e53a3
Adrien Regis <adrien.regis@gmail.com>
2023-11-16 14:55:14 +0100

    JS controller added to create carousel, English Page

---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="carousel"
+
+export default class extends Controller {
+  static targets = ["slide"];
+  index = 0;
+
+  connect() {
+    console.log("hey i'm here");
+    this.showSlide();
+    setInterval(() => {
+      this.index++;
+      if (this.index === this.slideTargets.length) {
+        this.index = 0;
+      }
+      this.showSlide();
+    }, 3000); // Change d'image toutes les 5 secondes, modifier au besoin
+  }
+
+  showSlide() {
+    this.slideTargets.forEach((slide, i) => {
+      if (i === this.index) {
+        slide.style.display = "block";
+      } else {
+        slide.style.display = "none";
+      }
+    });
+  }
+}
