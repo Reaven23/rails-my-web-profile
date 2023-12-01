aa4c762cdf7d8f74779a126aa7b6a0775afb3025
Adrien Regis <adrien.regis@gmail.com>
2023-12-01 10:09:33 +0100

    back to zero on the projects

---
+++ app/javascript/controllers/carousel_projet_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="carousel-projet"
+export default class extends Controller {
+  static targets = ["slide"];
+  index = 0;
+  
+  connect() {
+    console.log("hey i'm here");
+    this.showSlide();
+    setInterval(() => {
+      this.index++;
+      if (this.index === this.slideTargets.length) {
+        this.index = 0;
+      }
+      this.showSlide();
+    }, 3000);
+  }
+
+  showSlide() {
+    this.slideTargets.forEach((slide, i) => {
+      if (i === this.index) {
+        slide.style.display = "block";
+      } else {
+        slide.style.display = "none";
+      }
+    });
+  }
+}
