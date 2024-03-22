31de3e3a096862b046f77ffae9a970d9f5986700
Adrien Regis <adrien.regis@gmail.com>
2024-03-22 10:42:57 +0100

    retour v normale

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get "english", to: "pages#english"
   get '/telecharger-cv', to: 'cvchargers#telecharger_cv', as: 'telecharger_cv'
   get '/cv-interim', to: 'cvchargers#telecharger_cv_interim', as: 'cv_interim'
+
   resources :contact, only: %i[new create]
 end
