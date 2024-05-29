c41e5f78dba4a0c93efe59be1bae5a65c9677acb
Adrien Regis <adrien.regis@gmail.com>
2024-05-29 18:54:14 +0200

    new cv

--- app/controllers/cvchargers_controller.rb
+++ app/controllers/cvchargers_controller.rb
@@ -1,7 +1,7 @@
 class CvchargersController < ApplicationController
   def telecharger_cv
-    cv_path = Rails.root.join('public', 'Adrien_cv_fs.pdf')
-    send_file cv_path, filename: 'Adrien_cv_fs_cv.pdf', type: 'application/pdf', disposition: 'inline'
+    cv_path = Rails.root.join('public', 'CV_ADRIEN_REGIS_FS.pdf')
+    send_file cv_path, filename: 'CV_ADRIEN_REGIS_FS_cv.pdf', type: 'application/pdf', disposition: 'inline'
   end
 
   def telecharger_cv_interim
