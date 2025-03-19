cca58d53f7e8b7ca865bcbebbd7b7bfdb26de22c
Reaven23 <adrien.regis@gmail.com>
2025-03-19 05:52:45 +0100

    test functionnal

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,26 +15,37 @@ class PagesController < ApplicationController
 
   def test
 
-    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/paris?unitGroup=us&elements=datetimeEpoch%2Ctemp%2Cfeelslike%2Cconditions%2Cdescription%2Cicon&include=days%2Ccurrent&key=#{ENV["WEATHER_KEY"]}&contentType=json"
+    today = Date.today.strftime("%Y-%m-%d")
+    end_date = (Date.today + 15).strftime("%Y-%m-%d")
+
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/paris/#{today}/#{end_date}?unitGroup=us&elements=datetimeEpoch%2Ctemp%2Cfeelslike%2Cconditions%2Cdescription%2Cicon&include=obs%2Cdays&key=#{ENV["WEATHER_KEY"]}&contentType=json"
+
 
     begin
       response = JSON.parse(URI.parse(url).read)
+      puts JSON.parse(URI.parse(url).read).inspect
+
 
       if response["days"].present?
-        daily_weather = response["days"].map do |day|
+        today = Date.today
+
+        # Filtrer uniquement les jours >= aujourd'hui
+        @daily_weather = response["days"]
+                          .select { |day| Time.at(day["datetimeEpoch"]).utc.to_date >= today }
+                          .map do |day|
           {
-          "datetime" => Time.at(day["datetimeEpoch"]).utc.to_date.to_s,
-          "temp" => day["temp"],
-          "feelslike" => day["feelslike"],
-          "description" => day["description"],
-          "icon" => day["icon"]
+            "datetime" => Time.at(day["datetimeEpoch"]).utc.to_date.to_s,
+            "temp" => day["temp"],
+            "feelslike" => day["feelslike"],
+            "description" => day["description"].empty? ? "No description available" : day["description"],
+            "icon" => day["icon"]
           }
         end
 
-        puts daily_weather
+        puts @daily_weather
 
       else
-        daily_weather = []
+        @daily_weather = []
       end
 
     rescue StandardError => erreur
